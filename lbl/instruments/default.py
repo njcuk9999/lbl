bdd9a0a96e1c5fbfcdad80fbb69e8602a02e0664
Neil Cook <neil.james.cook@gmail.com>
2021-04-16 13:15:38 -0400

    merge etienne testzone changes

--- lbl/instruments/default.py
+++ lbl/instruments/default.py
@@ -228,6 +228,9 @@ class Instrument:
         header = self.set_hkey(header, 'KW_PDATE', value=Time.now().fits)
         # add which lbl instrument was used
         header = self.set_hkey(header, 'KW_INSTRUMENT', value=self.name)
+        # add the template velocity from CCF
+        header = self.set_hkey(header, 'KW_MODELVEL',
+                               value=outputs['MODEL_VELOCITY'])
         # ---------------------------------------------------------------------
         # Convert ref table dictionary to table
         # ---------------------------------------------------------------------
