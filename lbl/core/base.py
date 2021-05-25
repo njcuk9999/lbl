9fb7e439dc0b441da73ebcd8654ac0192ed8d812
Neil Cook <neil.james.cook@gmail.com>
2021-05-25 10:54:50 -0400

    update version/date after update

--- lbl/core/base.py
+++ lbl/core/base.py
@@ -16,8 +16,8 @@ import os
 # Define variables
 # =============================================================================
 __NAME__: str = 'base.py'
-__version__: str = '0.0.011'
-__date__: str = '2021-05-03'
+__version__: str = '0.0.012'
+__date__: str = '2021-05-25'
 __authors__: str = 'Neil Cook, Etienne Artigau'
 __package__: str = 'lbl'
 
