866840715a0a348e3facb01ee9e0a8def6405f22
Neil Cook <neil.james.cook@gmail.com>
2021-10-18 19:52:39 -0400

    add lbl_find - a tool to find input files and add them to the correct places

---
+++ lbl/recipes/lbl_find.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Find LBL input files for an instrument
+
+Created on 2021-10-18
+
+@author: cook
+"""
+import numpy as np
+
+from lbl.core import base
+from lbl.core import base_classes
+from lbl.core import io
+from lbl.instruments import select
+from lbl.science import general
+from lbl.resources import lbl_misc
+
+
+# =============================================================================
+# Define variables
+# =============================================================================
+__NAME__ = 'lbl_compute.py'
+__STRNAME__ = 'LBL Compute'
+__version__ = base.__version__
+__date__ = base.__date__
+__authors__ = base.__authors__
+# get classes
+InstrumentsList = select.InstrumentsList
+InstrumentsType = select.InstrumentsType
+ParamDict = base_classes.ParamDict
+LblException = base_classes.LblException
+log = base_classes.log
+# add arguments (must be in parameters.py)
+ARGS_FIND = [# core
+             'INSTRUMENT', 'CONFIG_FILE']
+DESCRIPTION_FIND = 'Find LBL input files for an instrument'
+
+# =============================================================================
+# Define functions
+# =============================================================================
+def main(**kwargs):
+    """
+    Wrapper around __main__ recipe code (deals with errors and loads instrument
+    profile)
+
+    :param kwargs: kwargs to parse to instrument - anything in params can be
+                   parsed (overwrites instrumental and default parameters)
+    :return:
+    """
+    # deal with parsing arguments
+    args = select.parse_args(ARGS_FIND, kwargs, DESCRIPTION_FIND)
+    # load instrument
+    inst = select.load_instrument(args, logger=log)
+    # get data directory
+    data_dir = io.check_directory(inst.params['DATA_DIR'])
+    # move log file (now we have data directory)
+    lbl_misc.move_log(data_dir, __NAME__)
+    # print splash
+    lbl_misc.splash(name=__STRNAME__, instrument=inst.name,
+                    cmdargs=inst.params['COMMAND_LINE_ARGS'], logger=log)
+    # run __main__
+    try:
+        namespace = __main__(inst)
+    except LblException as e:
+        raise LblException(e.message)
+    except Exception as e:
+        emsg = 'Unexpected lbl_compile error: {0}: {1}'
+        eargs = [type(e), str(e)]
+        raise LblException(emsg.format(*eargs))
+    # end code
+    lbl_misc.end(__NAME__, logger=log)
+    # return local namespace
+    return namespace
+
+
+def __main__(inst: InstrumentsType, **kwargs):
+    """
+    The main recipe function - all code dealing with recipe functionality
+    should go here
+
+    :param inst: Instrument instance
+    :param kwargs: kwargs to parse to instrument (only use if inst is None)
+                   anything in params can be parsed (overwrites instrumental
+                   and default parameters)
+
+    :return: all variables in local namespace
+    """
+    # -------------------------------------------------------------------------
+    # deal with debug
+    if inst is None or inst.params is None:
+        # deal with parsing arguments
+        args = select.parse_args(ARGS_FIND, kwargs, DESCRIPTION_FIND)
+        # load instrument
+        inst = select.load_instrument(args)
+        # assert inst type (for python typing later)
+        amsg = 'inst must be a valid Instrument class'
+        assert isinstance(inst, InstrumentsList), amsg
+    # -------------------------------------------------------------------------
+    # find input files for this instrument
+    inst.find_inputs()
+
+
+# =============================================================================
+# Start of code
+# =============================================================================
+if __name__ == "__main__":
+    # print hello world
+    ll = main()
+
+# =============================================================================
+# End of code
+# =============================================================================
