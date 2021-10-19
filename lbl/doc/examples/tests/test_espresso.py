4ba893a52df7e2b2bd056061f71641e40432f9cc
Neil Cook <neil.james.cook@gmail.com>
2021-10-19 16:48:18 -0400

    update test_espresso.py

--- lbl/doc/examples/tests/test_espresso.py
+++ lbl/doc/examples/tests/test_espresso.py
@@ -10,27 +10,29 @@ Created on 2021-10-18
 @author: artigau
 """
 from lbl import compil
-
+from lbl import compute
 
 # =============================================================================
 # Define variables
 # =============================================================================
+# define working directory
+working = '/scratch/lbl/data/espresso/'
 # create keyword argument dictionary
 keyword_args = dict()
 # add keyword arguments
 keyword_args['INSTRUMENT'] = 'ESPRESSO'
-keyword_args['DATA_DIR'] = '/Volumes/courlan/ESPRESSO'
+keyword_args['DATA_DIR'] = working
 keyword_args['TEMPLATE_SUBDIR'] = 'templates'
 keyword_args['BLAZE_FILE'] = 'dummy_blaze.fits'
 keyword_args['TEMPLATE_FILE'] = 'Template_LHS-1140.fits'
-keyword_args['PLOT'] = True
+keyword_args['PLOT'] = False
 keyword_args['PLOT_COMPUTE_CCF'] = keyword_args['PLOT']
 keyword_args['PLOT_COMPUTE_LINES'] = keyword_args['PLOT']
 keyword_args['PLOT_COMPIL_CUMUL'] = keyword_args['PLOT']
 keyword_args['PLOT_COMPIL_BINNED'] = keyword_args['PLOT']
 keyword_args['SKIP_DONE'] = False
-keyword_args['MASK_SUBDIR'] = '/Volumes/courlan/ESPRESSO/masks'
-keyword_args['INPUT_FILE'] = '/Volumes/courlan/ESPRESSO/science/LHS-1140-tc/ES*.fits'
+keyword_args['MASK_SUBDIR'] = working + 'masks'
+keyword_args['INPUT_FILE'] = working + 'science/LHS-1140-tc/ES*.fits'
 # add objects
 objs = ['LHS-1140']
 templates = ['LHS-1140']
@@ -43,8 +45,11 @@ num = 0
 # =============================================================================
 if __name__ == "__main__":
     # run compile
-    dict_tbl = compil(object_science=objs[num], object_template=templates[num],
-                      **keyword_args)
+    tbl1 = compute(object_science=objs[num], object_template=templates[num],
+                   **keyword_args)
+
+    tbl2 = compil(object_science=objs[num], object_template=templates[num],
+                  **keyword_args)
 
 # =============================================================================
 # End of code
