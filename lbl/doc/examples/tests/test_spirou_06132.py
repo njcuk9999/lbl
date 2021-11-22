debeb0e2a60445294ab795996bfdca668836f1f4
Neil Cook <neil.james.cook@gmail.com>
2021-11-22 10:05:34 -0500

    add some tests for spirou 0.6

---
+++ lbl/doc/examples/tests/test_spirou_06132.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Test of the LBL for Spirou (FP + Gl699) + drift correction
+
+Created on 2021-10-18
+
+@author: artigau, cook
+"""
+from lbl import clean
+from lbl import compil
+from lbl import compute
+from lbl import mask
+from lbl import template
+from lbl.recipes import lbl_find
+
+# =============================================================================
+# Define variables
+# =============================================================================
+# define working directory
+working = '/scratch3/lbl/bin/lbl/lbl/doc/examples/'
+# create keyword argument dictionary
+fp_config_file = working + 'spirou_fp_config_06132.yaml'
+gl699_config_file = working + 'spirou_gl699_config_06132.yaml'
+
+
+# =============================================================================
+# Start of code
+# =============================================================================
+if __name__ == "__main__":
+    # # run clean (reset everything)
+    _ = clean(config_file=fp_config_file)
+    _ = clean(config_file=gl699_config_file)
+    # find files
+    _ = lbl_find.main(instrument='SPIROU', config_file=gl699_config_file)
+    # # run template
+    tbl0a = template(config_file=fp_config_file)
+    tbl0b = template(config_file=gl699_config_file)
+    # # run mask code
+    tbl1a = mask(config_file=fp_config_file)
+    tbl1b = mask(config_file=gl699_config_file)
+    # run compute
+    tbl2a = compute(config_file=fp_config_file)
+    tbl2b = compute(config_file=gl699_config_file)
+    # run compile
+    tbl3a = compil(config_file=fp_config_file)
+    tbl3b = compil(config_file=gl699_config_file)
+
+# =============================================================================
+# End of code
+# =============================================================================
