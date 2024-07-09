1e41e47957296e1b5f71b79846422bcb1b965e31
Neil Cook <neil.james.cook@gmail.com>
2024-07-09 11:46:54 -0400

    [LBL] Add a flag to flag whether files are or are not blaze corrected "BLAZE_CORRECTED" and add to wrap creation

--- lbl/core/base.py
+++ lbl/core/base.py
@@ -15,8 +15,8 @@ from astropy.time import Time, TimeDelta
 # Define variables
 # =============================================================================
 __NAME__: str = 'base.py'
-__version__: str = '0.63.009'
-__date__: str = '2024-07-08'
+__version__: str = '0.63.010'
+__date__: str = '2024-07-09'
 __authors__: str = ('Neil Cook, Etienne Artigau, Charles Cadieux, '
                     'Thomas Vandal, Ryan Cloutier, Pierre Larue')
 __package__: str = 'lbl'
