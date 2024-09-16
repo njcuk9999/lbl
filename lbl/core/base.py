c2feac514dec86ff3d2889d882245dd4c92d9894
Neil Cook <neil.james.cook@gmail.com>
2024-09-16 08:58:41 -0400

    [LBL] Update version and date

--- lbl/core/base.py
+++ lbl/core/base.py
@@ -15,8 +15,8 @@ from astropy.time import Time, TimeDelta
 # Define variables
 # =============================================================================
 __NAME__: str = 'base.py'
-__version__: str = '0.63.14'
-__date__: str = '2024-08-19'
+__version__: str = '0.64.0'
+__date__: str = '2024-09-16'
 __authors__: str = ('Neil Cook, Etienne Artigau, Charles Cadieux, '
                     'Thomas Vandal, Ryan Cloutier, Pierre Larue')
 __package__: str = 'lbl'
