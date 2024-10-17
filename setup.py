f26d2982bb3889a0c14dff0114e44c117f84831e
Neil Cook <neil.james.cook@gmail.com>
2024-10-17 09:44:32 -0400

    [LBL] update log, version etc

--- setup.py
+++ setup.py
@@ -1,2 +1,20 @@
+import sys
 from setuptools import setup
-setup()
+
+def get_version() -> str:
+    """
+    Get the version from the version file
+    :return:
+    """
+    # try to open version file
+    try:
+        with open('version.txt', 'r') as vfile:
+            vtext = vfile.readlines()
+    except Exception as e:
+        print('Error: Could not read version file')
+        print('Error: {0}'.format(e))
+        sys.exit(1)
+    # return version
+    return vtext[0]
+
+setup(version=get_version())
