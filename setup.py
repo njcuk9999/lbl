9512f8aaffcc0e71a994e26aa8fb84ec70213bc0
Neil Cook <neil.james.cook@gmail.com>
2025-02-25 11:59:42 -0500

    [LBL] update to python 3.11 (and use pyproject.toml)

--- setup.py
+++ setup.py
@@ -20,29 +20,4 @@ def get_version() -> str:
     # return version
     return vtext[0]
 
-def load_requirements() -> list:
-    """
-    Load requirements from file
-    :return:
-    """
-    requirements = 'requirements.txt'
-    # storage for list of modules
-    modules = []
-    # open requirements file
-    with open(requirements, 'r') as rfile:
-        lines = rfile.readlines()
-    # get modules from lines in requirements file
-    for line in lines:
-        if len(line) == '':
-            continue
-        if line.startswith('#'):
-            continue
-        else:
-            modules.append(line)
-    # return modules
-    return modules
-
-
-setup(version=get_version(),
-      include_package_data=True,
-      install_requires=load_requirements())
+setup(version=get_version())
